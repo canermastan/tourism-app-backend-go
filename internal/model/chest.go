af623748182e70cbe8e1a3ff50f574bfa269d7f9
FurkanZenginn <furkanrichdevelopper@gmail.com>
2025-01-18 13:27:43 +0300

    Fixed all

--- internal/model/chest.go
+++ internal/model/chest.go
@@ -2,6 +2,14 @@ package model
 
 type Chest struct { // chests
 	ID      int64 `gorm:"primaryKey"`
-	Gain    int64 `gorm:"not null"`
+	Gain    int64 `json:"gain" gorm:"not null"`
 	PlaceID int64 `json:"place_id" gorm:"not null"`
+}
+
+type ValidationError struct {
+	Message string `json:"message"`
+}
+
+func (e *ValidationError) Error() string {
+	return e.Message
 }
